f8d5a558b6b7da213fdd621aa93593290e8715bb
Fawad Ali <fawadali@Fawads-MacBook-Pro.local>
2021-06-30 20:56:25 -0700

    added user_info on config.py. This is a dictionary that contains username, password, and port configuration

--- config.py
+++ config.py
@@ -2,3 +2,9 @@ stripe_keys = {
     'secret_key': "sk_test_4eC39HqLyjWDarjtT1zdp7dc",
     'publishable_key': "pk_test_TYooMQauvdEDq54NiTphI7jx"
 }
+
+user_info = {
+	"username" : "username",
+	"password" : "password",
+	"port_config": 80
+}
